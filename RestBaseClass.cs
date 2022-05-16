52931c746edf32df40270a07db74307d1086164c
Jonathan Souza <jonathancsouza5@gmail.com>
2022-05-16 12:59:07 -0700

    Removed usings

--- RestBaseClass.cs
+++ RestBaseClass.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.InteropServices.ComTypes;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace RestTestProject
 {
